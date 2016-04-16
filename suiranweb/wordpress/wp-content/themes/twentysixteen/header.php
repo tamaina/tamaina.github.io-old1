57f069846d4784926a228f21e52776b332fd2d52
tamaina <tamainasimu@gmail.com>
2016-04-16 21:05:54 +0900

    つづき

--- suiranweb/wordpress/wp-content/themes/twentysixteen/header.php
+++ suiranweb/wordpress/wp-content/themes/twentysixteen/header.php
@@ -19,53 +19,25 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
 	<?php wp_head(); ?>
+
+
+
+
+
          <base href="http://localhost/suiranweb/">
          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
          <meta name="viewport" content="width=device-width">
-           <title>翠巒祭プレス - <?php bloginfo('name'); ?></title>
-            <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
-              <!--Import Google Icon Font-->
-            <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-              <!-- Compiled and minified CSS -->
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-            <link rel="stylesheet" href="./css/style.css">
-			<link rel="stylesheet" href="./css/press_style.css">
-        </head>
-        <body>
-              <!--Import jQuery before materialize.js-->
-            <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-              <!-- Compiled and minified JavaScript -->
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-<div class="navbar-fixed">
-  <nav class="navigationbar light-blue darken-3">
-    <div class="nav-wrapper container">
-      <a href="#" class="brand-logo">翠巒祭</a>
-       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="./">Top</a></li>
-        <li><a href="./about/">About</a></li>
-        <li><a href="./events/">Events</a></li>
-        <li><a href="./goods/">Goods</a></li>
-        <li><a href="./food/">Food</a></li>
-        <li><a href="./access/">Access</a></li>
-        <li><a href="./gallery/">Garally</a></li>
-        <li><a href="./links/">Links</a></li>
-        <li><a href="./game/">Game</a></li>
-      </ul>
-      <ul id="slide-out" class="side-nav">
-        <li><a href="./">Top</a></li>
-        <li><a href="./about/">About</a></li>
-        <li><a href="./event/">Event</a></li>
-        <li><a href="./goods/">Goods</a></li>
-        <li><a href="./food/">Food</a></li>
-        <li><a href="./access/">Access</a></li>
-        <li><a href="./gallery/">Garally</a></li>
-        <li><a href="./links/">Links</a></li>
-        <li><a href="./game/">Game</a></li>
-      </ul>
-      <a href="javascript:void(0);" data-activates="slide-out" class="button-collapse container"><i class="fa fa-bars" aria-hidden="true"></i>
-</i>
-</a>
-    </div>
-  </nav>
-</div>
+         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+         <meta name="viewport" content="width=device-width">
+         <!--Import jQuery before materialize.js-->
+         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+         <!-- Compiled and minified JavaScript -->
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+<script type="text/javascript">
+$(function(){
+$("#header").load("./parts/header.html");
+$("#footer").load("./parts/footer.html");
+})
+</script>
+<div id="header"></div>
 <div class="container row">
