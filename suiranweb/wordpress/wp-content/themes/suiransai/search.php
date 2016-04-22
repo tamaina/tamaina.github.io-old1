f770ddf9139e8665336f1978b12b2bbed68b6451
tamaina <tamainasimu@gmail.com>
2016-04-23 07:59:34 +0900

    nau

--- suiranweb/wordpress/wp-content/themes/suiransai/search.php
+++ suiranweb/wordpress/wp-content/themes/suiransai/search.php
@@ -12,8 +12,12 @@ get_header(); ?>
 <div class="col s12 l8">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php if ( have_posts() ) : ?>
-		<?php while ( have_posts() ) : the_post(); ?>
+
+	<?php $paged = get_query_var('paged'); ?>
+	<?php query_posts("posts_per_page=10&paged='.$paged"); ?>
+	<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+	
+	
 		<div class="row card teal lighten-5">
 		<div class="card-content col s12">
 			<p><?php the_category(); ?>投稿者:<?php the_author_posts_link(); ?> ／ <?php the_tags(); ?> ／ 作成:<?php the_time('Y年m月d日（D）'); ?>
@@ -23,28 +27,24 @@ get_header(); ?>
 			<?php if ( has_post_thumbnail() ) : ?>
 			<div style="width:100px;height:100px;float:left;"><?php the_post_thumbnail( array(100,100) ); ?></div>
 			<div style="float:right;"><?php echo mb_substr($post->post_content,0,200).'...'; ?></div>
+			</div>
+			<?php else: ?>
+			<div style="float:none;"><?php echo mb_substr($post->post_content,0,200).'...'; ?></div>
 			<?php endif; ?>
 			</a>
-	</div>
-</div>
-		<?php endwhile; endif; ?>
+		</div>
+		</div>
 		
-	<?php $paged = get_query_var('paged'); ?>
-
-	<?php query_posts("posts_per_page=10&paged=$paged"); ?>
-
-	 
-
-	<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-
-	
-
+		
 	<?php endwhile; ?>
-
 	<?php else: ?>
-
+	
+			<p>条件に当てはまる記事はありませんでした。</p>
+			
 	<?php endif; ?>
 
+	<?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+	<?php wp_reset_query(); ?>
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 </div>
--- suiranweb/wordpress/wp-content/themes/suiransai/single.php
+++ suiranweb/wordpress/wp-content/themes/suiransai/single.php
@@ -12,17 +12,12 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		<?php if ( have_posts() ) : the_post(); ?>
-		<div class="card teal lighten-5">
+		<div class="card grey lighten-5">
 		<div class="card-content">
 			<p><?php the_category(); ?>投稿者:<?php the_author_posts_link(); ?> ／ <?php the_tags(); ?> ／ 作成:<?php the_time('Y年m月d日（D）'); ?>
 			</p>
 			<h2><span class="card-title entry_title"><?php the_title(); ?></span></h2>
-			<?php if ( has_post_thumbnail() ) : ?>
-			<div style="width:100px;height:100px;float:left;"><?php the_post_thumbnail( array(100,100) ); ?></div>
-			<div style="float:right;"><?php the_content(); ?></div>
-			<?php else: ?>
 			<div style="float:none;"><?php the_content(); ?></div>
-			<?php endif; ?>
 		</div>
 		<?php endif; ?>
 		</div>
