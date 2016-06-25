f04e57ad899af9fb77e077956575c9c431059a06
tamaina <tamainasimu@gmail.com>
2016-06-25 19:10:52 +0900

    _

--- suiranweb/wordpress/wp-content/themes/suiransai/header.php
+++ suiranweb/wordpress/wp-content/themes/suiransai/header.php
@@ -13,13 +13,14 @@
          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
          <meta name="viewport" content="width=device-width">
          <!--Import jQuery before materialize.js-->
-         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+         <script type="text/javascript" src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
          <!-- Compiled and minified JavaScript -->
          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
 <script type="text/javascript">
 $(function(){
-$("#header").load("./parts/header.html");
-$("#footer").load("./parts/footer.html");
+$("#HeaderInHEAD").load("../parts/header1.html");
+$("#HeaderInBODY").load("../parts/header2.html");
+$("#footer").load("../parts/footer.html");
 })
 </script>
 <?php
@@ -28,6 +29,8 @@ $("#footer").load("./parts/footer.html");
 		get_template_part('content');
 	} ?>
 </div>
-<div id="header"></div>
+
+<div id="HeaderInHEAD"></div></head>
+<body><div id="HeaderInBODY"></div>
 <div class="container row">
   <h1 class="presstitle"><a href="./wordpress/" style="color:black;">Press</a></h1>
