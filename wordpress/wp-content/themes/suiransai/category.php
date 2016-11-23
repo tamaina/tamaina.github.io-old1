4568fec577b938a1231e2ee161c96cfe794b38bf
tamaina <tamainasimu@gmail.com>
2016-11-23 13:52:22 +0900

    relax

--- wordpress/wp-content/themes/suiransai/category.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-<h2><i class="fa fa-book" aria-hidden="true"></i> カテゴリ:<?php single_cat_title(); ?></h2>
-<div class="card-panel"><?php echo category_description(); ?></div>
-<?php get_template_part('thumb'); ?>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
--- wordpress/wp-content/themes/suiransai/tag.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-<h2><i class="fa fa-tag" aria-hidden="true"></i> タグ:<?php single_cat_title(); ?></h2>
-<div class="card-panel"><?php echo tag_description(); ?></div>
-<?php get_template_part('thumb'); ?>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
