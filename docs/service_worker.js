abae1df25cfcae8a01c88623e5cac5d6c5a067a1
tamaina <tamaina@hotmail.co.jp>
2017-08-26 22:31:07 +0900

    fix_disqus

--- docs/service_worker.js
+++ docs/service_worker.js
@@ -1,4 +1,4 @@
-var version = '0.1.7';
+var version = '0.2.0';
 var cachepages = [
     `info.json`,
     "./",
--- theme/js/pjax_init.js
+++ theme/js/pjax_init.js
@@ -1,6 +1,6 @@
 var Pjax = require('pjax-api').Pjax;
 
-new Pjax({  areas: ['#main',"#disqus_wrap"], update: { head: 'meta, link' }  });
+new Pjax({  areas: ['#main, #disqus_wrap'], update: { head: 'meta, link' }  });
 
 $(document).on('pjax:ready',function(){
     hljs.initHighlighting.called = false;
